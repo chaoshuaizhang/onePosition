c85e5b76782170639df12bfaf24f55e14d9a1d75
zhangchaoshuai <zcs451283@163.com>
2016-12-06 23:35:57 +0800

    使用fragtabhost实现Frag切换

---
+++ onePosition/app/src/main/java/cn/shopin/oneposition/fragments/BalloonFragment.java
@@ -0,0 +1,32 @@
+package cn.shopin.oneposition.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.shopin.oneposition.R;
+
+/**
+ * Created by zcs on 2016/12/5.
+ */
+public class BalloonFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag_balloon, null);
+        return view;
+    }
+}
